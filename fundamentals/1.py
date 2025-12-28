a8ad1cc81f04eacdae51bf597a88035f0ac2a443
beginToCode-1 <burhankhan7059@gmail.com>
2025-12-28 22:03:44 +0500

    IF statements are also cool

--- fundamentals/1.py
+++ fundamentals/1.py
@@ -1,5 +1,8 @@
 print ("Hello World, this is me Burhan, learning Python!")
 a = 10
 b = 20
-c = a + b
-print ("The sum of a and b is:", c)
+
+if a > b:
+    print ("a is greater than b")
+else:
+    print ("b is greater than a")
