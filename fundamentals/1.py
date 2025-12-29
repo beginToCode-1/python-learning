6a2cd1af9bfdbbefe16b82a7a06739dc9ab37bb5
beginToCode-1 <burhankhan7059@gmail.com>
2025-12-29 17:38:36 +0500

    Just hanging around

--- fundamentals/1.py
+++ fundamentals/1.py
@@ -2,7 +2,13 @@ print ("Hello World, this is me Burhan, learning Python!")
 a = 10
 b = 20
 
+
+name = "Burhan"
+age = 25
+print ("My name is", name, "and I am", age, "years old.")
+
 if a > b:
     print ("a is greater than b")
 else:
     print ("b is greater than a")
+    
