652c63d9a2302e255551834db6e5c60db44c7a25
beginToCode-1 <burhankhan7059@gmail.com>
2026-01-05 22:37:57 +0500

    strings, boolean, numbers

--- fundamentals/2.py
+++ fundamentals/2.py
@@ -1,2 +1,20 @@
 #numbers in python
 total = 5 + 10 - 3 * 2 / 4
+print("The total is:", total)   
+power = 2 ** 3
+print("2 to the power of 3 is:", power)
+
+#strings in python
+firstName = "Burhan"
+lastName = "Khan"
+fullName = firstName + " " + lastName
+print("Full Name:", fullName)
+#length of string
+dots = "..." * 30
+print("Dots:", dots)
+print(len(dots))
+#boolean in python
+is_false = False
+age = 20
+can_vote = age >= 18
+print("Can vote:", can_vote)
