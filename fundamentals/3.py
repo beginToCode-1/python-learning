b16184c0a6932ea7369f99d358880909286b16f9
beginToCode-1 <burhankhan7059@gmail.com>
2026-01-07 16:29:14 +0500

    if statemenst

---
+++ fundamentals/3.py
@@ -0,0 +1,8 @@
+temperature = int(input("Enter a number: "))
+if temperature > 30:
+    print("It's a hot day")
+    print("It is Recommended to drink plenty of water!!")
+elif temperature > 20:
+    print("It's a nice day, not too hot, not too cold, enjoy your day!")
+else: 
+    print("It's cold day, Make sure to wear warm clothes!!")
