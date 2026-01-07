388d661a2a3117829fcd82478f764621395d65b5
beginToCode-1 <burhankhan7059@gmail.com>
2026-01-07 16:03:59 +0500

    Strings

--- fundamentals/2.py
+++ fundamentals/2.py
@@ -5,10 +5,13 @@ power = 2 ** 3
 print("2 to the power of 3 is:", power)
 
 #strings in python
-firstName = "Burhan"
-lastName = "Khan"
-fullName = firstName + " " + lastName
-print("Full Name:", fullName)
+#firstName = "Burhan"
+#lastName = "Khan"
+#fullName = firstName + " " + lastName
+#print("Full Name:", fullName)
+name = "Burhan "
+string = f"Hello, this is me {name}"
+
 #length of string
 dots = "..." * 30
 print("Dots:", dots)
