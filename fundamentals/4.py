b0021d52eaf94e10bdaad9d3a892036d3b480bcb
beginToCode-1 <burhankhan7059@gmail.com>
2026-01-09 14:45:15 +0500

    for loops

--- fundamentals/4.py
+++ fundamentals/4.py
@@ -4,4 +4,8 @@ for i in range(5):
 #letter in my name
 name = "Burhan Rauf"
 for letter in name:
-    print(letter)
+    print(letter)  
+#sum of first 10 numbers
+total = 0
+for num in range(1, 11):
+    total += num
