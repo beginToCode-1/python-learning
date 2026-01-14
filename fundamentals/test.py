f5a666e70ace106389d7ea3a5d6ab0172b41eb9a
beginToCode-1 <burhankhan7059@gmail.com>
2026-01-14 20:28:59 +0500

    beginner level list questions

---
+++ fundamentals/test.py
@@ -0,0 +1,11 @@
+my_list = [1,2,3,4,5,5,3,2]
+print(my_list)
+
+
+my_list.append(6)
+print(my_list)
+unique = set(my_list)
+print(unique)
+print(my_list.count(3))
+print(my_list[1])
+print(my_list[-1])
