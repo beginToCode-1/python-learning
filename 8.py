380f66db7f971010d7b5c40400a4eeffcf943a03
beginToCode-1 <burhankhan7059@gmail.com>
2026-01-14 18:00:09 +0500

    diction

--- 8.py
+++ 8.py
@@ -4,10 +4,5 @@ car = {
     "model": "Mustang",
     "year": 1964,
     "color": "red"
-}
-print("Keys::",car.keys())
-print("Values::",car.values())
-print("items with keys::",car.items())
-print(car.update({"color" : "silver gray"}))
-print(car)
-print(car.get("brand"))
+} 
+print(car.values())  
