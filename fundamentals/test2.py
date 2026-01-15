a30b939133c9033b4b24b61b51d8aa9451f32df7
beginToCode-1 <burhankhan7059@gmail.com>
2026-01-15 18:40:46 +0500

    testing my dictionary skills

--- fundamentals/test2.py
+++ fundamentals/test2.py
@@ -4,6 +4,14 @@ my_self_dictionary = {
     "age": 21,
 }
 print(my_self_dictionary.get("name"))
+
+#adding new key-value pairs
 my_self_dictionary["Status"] = "Student"
 my_self_dictionary["city"] = "Islamabad"
+
+#updating age
+my_self_dictionary["age"] = 22
+print(my_self_dictionary)
+
+#updated dictionary values only
 print(my_self_dictionary.values())
