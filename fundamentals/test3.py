06ac2ceb893c1b655ebffa50fbb3306ee0e14470
beginToCode-1 <burhankhan7059@gmail.com>
2026-01-16 18:36:01 +0500

    doing sets

---
+++ fundamentals/test3.py
@@ -0,0 +1,11 @@
+#sets
+my_self_set = {"apple", "banana", "cherry"}
+print(my_self_set)
+#adding new item
+print(my_self_set.add("orange"))
+#removing item
+print(my_self_set.remove("banana"))
+#updated set
+print(my_self_set)
+#clearing set
+my_self_set.clear()
