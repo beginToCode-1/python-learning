02a027e099d7928d9b3880babf48db60a23a8986
beginToCode-1 <burhankhan7059@gmail.com>
2026-01-18 21:43:34 +0500

    function

---
+++ fundamentals/function.py
@@ -0,0 +1,15 @@
+#functions
+def greetings ():
+    print("Hello!")
+    print("Welcome to the Python programming world.")
+
+
+
+greetings()
+
+
+def addNumbers (num1, num2) :
+    sum = num1 + num2
+    return sum
+
+print(addNumbers(5, 10))
